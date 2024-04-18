0427aea36842cd112b3cbefa3504b71ff647d0df
Mugisha Regis <mugisharegis72@gmail.com>
2024-04-18 11:33:02 +0200

    feat(banner): add banner component in Home

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bayut-production.s3.eu-central-1.amazonaws.com",
+        pathname: "/image/145426814/33973352624c48628e41f2ec460faba4",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
