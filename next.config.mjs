dd574493035aa61c365157e1005e304d29b261b5
Mugisha Regis <mugisharegis72@gmail.com>
2024-04-19 10:40:54 +0200

    config: configure image.remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "bayut-production.s3.eu-central-1.amazonaws.com",
-        pathname: "/image/145426814/33973352624c48628e41f2ec460faba4",
       },
     ],
   },
